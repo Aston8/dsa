375001238f6357a4dc9959d5293d84497a122239
Aston Monteiro <astonmonteiro08@gmail.com>
2024-09-07 09:42:28 +0530

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct queue{
+    int size;
+    int r;
+    int f;
+    int *arr;
+};
+
+int isFull(struct queue *q){
+    if(q->r == q->size-1 ){
+        return 1;
+    }
+        return 0;
+    
+}
+
+int isempty(struct queue *q){
+    if(q->f == q->r ){
+        return 1;
+    }
+        return 0;
+    
+}
+
+void enqueue(struct queue *q,int val){
+    if(isFull(q)){
+        printf("queue is full");
+    }else{
+        q->r++;
+        q->arr[q->r]=val;
+    }
+}
+
+int dequeue(struct queue *q){
+    if(isempty(q)){
+        printf("queue is empty");
+    }else{
+        int a;
+        q->f++;
+        a=q->arr[q->f];
+        return a;
+    }
+}
+
+
+
+
+int main(){
+    struct queue q;
+    q.size =10;
+    q.r=-1;
+    q.f=-1;
+    q.arr=(int *)malloc(q.size*sizeof(int));
+
+    printf("\n%d",isempty(&q));    
+    printf("\n%d",isFull(&q));
+    printf("\nAfter operation\n");
+   
+    enqueue(&q,56);
+    enqueue(&q,45);
+
+    printf("%d dequeued element \n",dequeue(&q));
+    
+    printf("\n%d",isempty(&q));
+    printf("\n%d",isFull(&q));
+
+    return 0;
+}
